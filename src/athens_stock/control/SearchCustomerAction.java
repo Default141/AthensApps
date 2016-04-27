648d278b23739843432b69056c7405871916f519
Chayut <chayut.cs@mail.kmutt.ac.th>
2016-04-27 18:53:37 +0700

    Create MVC package

---
+++ src/athens_stock/control/SearchCustomerAction.java
@@ -0,0 +1,23 @@
+package athens_stock.control;
+
+
+import javax.swing.*;
+
+import java.awt.event.ActionEvent;
+
+
+/**
+ * Created by Chayut on 19-Apr-16.
+ */
+public class SearchCustomerAction extends AbstractAction {
+
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+
+
+
+    }
+}
---
+++ src/athens_stock/model/Service.java
@@ -0,0 +1,38 @@
+package athens_stock.model;
+
+
+import org.jdatepicker.impl.JDatePickerImpl;
+
+import javax.swing.*;
+import java.util.ArrayList;
+
+/**
+ * Created by Chayut on 25-Mar-16.
+ */
+public interface Service {
+
+
+    ArrayList<Customer> searchCustomers(String sql);
+
+
+    String datePickerFormat(JDatePickerImpl datePicker);
+    boolean validateCustomer(Integer id);
+    String whoBook(String room, String timeStampIn, String timeStampOut);
+    boolean onService(Integer roomNumber);
+
+    int bookIdCreator();
+
+    void insertBook(String sql);
+    void cancelBook(String sql);
+
+    void checkIn(String id, String roomNumber);
+    void checkOut(int logId, String roomNumber);
+    double calculatePrice(String roomNumber, int numberOfDay);
+    void printOutInvoice(int logId, int numberOfDay, double price);
+
+    int getLatestLogId(String roomNumber);
+    int getNumberOfDay(int logId);
+
+    public int customerIdCreator();
+    boolean addCustomer(JTextField nameTextField, JTextField lastNameTextField, JTextField telField, JTextField addField, JComboBox sexBox);
+}
