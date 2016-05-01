b26167b5e0d2b2a4a478b7783d417914fccff5e6
BoyBurin <boy-burin@hotmail.com>
2016-05-01 19:26:43 +0700

    Create Order item controller

---
+++ src/athens_stock/control/OrderItemController.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package athens_stock.control;
+
+import athens_stock.view.OrderItem;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ *
+ * @author Burin
+ */
+public class OrderItemController {
+    private OrderItem orderitem;
+    
+    public OrderItemController(){
+        orderitem = new OrderItem();
+        orderitem.setVisible(true);
+        setOrderItemAction();
+    }
+    
+    private void setOrderItemAction(){
+        orderitem.setHomeActiom(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                MenuSalesController menu = new MenuSalesController();
+                orderitem.dispose();
+            }
+        });
+    }
+    
+    
+}
