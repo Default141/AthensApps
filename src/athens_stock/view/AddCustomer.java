95c0c0e5afe26215914c59fed87bc15183789359
lullaby <ngunngun>
2016-05-23 23:05:45 +0700

    fix AddCustomer GUI

--- src/athens_stock/view/AddCustomer.java
+++ src/athens_stock/view/AddCustomer.java
@@ -38,6 +38,7 @@ public class AddCustomer extends JFrame {
         btnBack.addActionListener(action);
     }
 
+
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
