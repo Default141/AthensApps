94a86345a90159e540cd4960cf477c712bbb8cb2
Chayut <chayut.cs@mail.kmutt.ac.th>
2016-05-24 05:51:39 +0700

    order WH done

---
+++ src/athens_stock/buttonAction/btnDeliverAction.java
@@ -0,0 +1,47 @@
+package athens_stock.buttonAction;
+
+import athens_stock.model.DBMethod;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+
+import static javax.swing.JOptionPane.YES_NO_OPTION;
+
+/**
+ * Created by Chayut on 24-May-16.
+ */
+
+public class btnDeliverAction extends AbstractAction {
+    private JTable table;
+    private JButton btnSearch;
+    DBMethod db = new DBMethod();
+
+    public btnDeliverAction(JTable table, JButton btnSearch) {
+        this.table = table;
+        this.btnSearch = btnSearch;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        for (int i = 0; i < table.getRowCount(); i++) {
+            Boolean isSelect;
+            int orderId = 0;
+            if (table.getValueAt(i, 0) == null) {
+                isSelect = false;
+                continue;
+            } else {
+                isSelect = (Boolean) table.getValueAt(i, 0);
+                orderId = (Integer) table.getValueAt(i, 1);
+            }
+
+            System.out.println(isSelect);
+            if (isSelect) {
+                int answer = JOptionPane.showConfirmDialog(null, "Confirm Delivered.", "Delivered", YES_NO_OPTION);
+                if (answer == JOptionPane.YES_OPTION) {
+                    db.deliver(orderId);
+                }
+            }
+        }
+        btnSearch.doClick();
+    }
+}
